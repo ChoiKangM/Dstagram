b92b2f016ddabae6c7865480b2ed9d462c75ac62
kei01138 <choikm3847@gmail.com>
2019-08-05 13:17:12 +0900

    hello Dstagram

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
